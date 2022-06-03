569c45aa936ee43e993deee82a0dc88e5be6fe05
MrClock8163 <jz.zalan@gmail.com>
2022-06-03 13:15:45 +0200

    Update n_inspect.py
    The inspector nodes now have generated IDs by default

--- nodes/n_inspect.py
+++ nodes/n_inspect.py
@@ -22,10 +22,26 @@ class MCFG_N_Inspect(Node, n_tree.MCFG_N_Base):
     )
     name: bpy.props.StringProperty(
         name = "Name",
-        description = "Identifier of inspector node",
+        description = "Custom identifier name of inspector node",
         default = ""
     )
+    inspect_id: bpy.props.IntProperty(
+        name = "Inspector ID",
+        description = "",
+        default = 0,
+        min = 0,
+        max = 1000
+    )
     
+    # Custom function
+    def GenerateInspectID(self,context):
+        inspect_id_max = self.inspect_id
+        for node in context.space_data.node_tree.nodes:
+            if node.bl_idname == "MCFG_N_Inspect":
+                inspect_id_max = max(node.inspect_id,inspect_id_max)
+        
+        return (inspect_id_max + 1)
+        
     # Standard functions
     def draw_label(self):
         return "Inspector"
@@ -34,15 +50,31 @@ class MCFG_N_Inspect(Node, n_tree.MCFG_N_Base):
         self.customColor()
         
         self.inputs.new('MCFG_S_Universal', "Value")
+        
+        self.inspect_id = self.GenerateInspectID(bpy.context)
 
     def draw_buttons(self, context, layout):
+        box = layout.box()
+        box.label(text=("ID: " + str(self.inspect_id)))
         layout.prop(self,"name")
         layout.prop(self,"active")
+
+    def copy(self, node):
+        print("Copying from node ", node)
+        self.inspect_id = self.GenerateInspectID(bpy.context)
         
     # Custom functions
+    def GetInspectID(self):
+        name = str(self.inspect_id)
+        
+        if self.name.strip() != "":
+            name = self.name.strip()
+            
+        return name
+    
     def inspect(self):
         if len(self.inputs[0].links) != 1 or not self.active:
             return
             
-        print("==== Inspector output (ID: " + self.name.strip() + ") ====")
+        print("==== Inspector output (ID: " + self.GetInspectID() + ") ====")
         self.inputs[0].links[0].from_node.inspect()
